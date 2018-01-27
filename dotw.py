c586b3185d0759d39d86624be4425755759e1dff
skinnyjay <jaredpyrothomas@gmail.com>
2018-01-27 14:39:53 -0600

    minor changes to readme

--- dotw.py
+++ dotw.py
@@ -2,9 +2,7 @@
 """
 dotw.py
 ~~~~~~~
-
 Python based calculator that determines the weekday of a given day
-
 Credit: https://blog.artofmemory.com/how-to-calculate-the-day-of-the-week-4203.html
 """
 
@@ -65,10 +63,8 @@ def calc_dotw(given_month=0,given_day=0,given_year=1749):
 	# Promt user for mm/dd/ccyy within the range of the Gregorian Calendar
 	while given_month not in range(1,13):
 		given_month = int(input("What month?(1-12): "))
-
 	while given_day not in range(1,(length_month[given_month]+1)):
 		given_day = int(input("What day?  (1-31): "))
-
 	while given_year < 1750:
 		given_year = int(input("What year? (ccyy): "))
 
@@ -76,7 +72,6 @@ def calc_dotw(given_month=0,given_day=0,given_year=1749):
 	value_mm = value_month[given_month]
 	year = str(given_year)
 
-	# Calculate Value for Year (ccyy)
 	cc = int(year[:2]) # Century Code value_cc for part of year
 	value_cc = value_century[cc]
 
@@ -84,8 +79,7 @@ def calc_dotw(given_month=0,given_day=0,given_year=1749):
 	yy_div4  = yy // 4
 	value_yy = (yy + yy_div4) % 7
 
-	# consider leap_mm and leap_yy
-	is_LeapYear = False
+	is_LeapYear = False # Consider leap_mm and leap_yy
 	value_ly = 0
 	if given_month < 3:
 		if given_year % 4 == 0:
@@ -103,26 +97,15 @@ def calc_dotw(given_month=0,given_day=0,given_year=1749):
 	else:
 		is_LeapYear = False
 
-	# value_year of using yy, cc and ly
-	value_year = (value_cc + value_yy + value_ly)
-
-	# total
 	total = 0
-	total += value_mm
-	total += value_day
-	total += value_year
+	value_year = (value_cc + value_yy + value_ly)
+	total = value_mm + value_day + value_year
 
-	# Optional user side math
-	#print("\n" + " {} / {} / {}".format(given_month, given_day, given_year))
-	#print(" {} +  {} +    {} = {}".format(value_mm, value_day, value_year, total))
-
-	# Ensure final total (day) fits within week structure
-	final_total = total
-	if final_total > 6:
-		final_total = final_total % 7
-
-	day = day_of_week[final_total]
+	if total > 6:
+		total = total % 7
+	day = day_of_week[total]
 	return day
 
+
 if __name__=="__main__":
-	print("\n{}\n".format(calc_dotw()))
+	print("\n{}\n".format(calc_dotw(1,29,1993)))
