8d9fdf0f88413ffbf4b25de86dab8fb3d8716589
skinnyjay <jaredpyrothomas@gmail.com>
2018-01-27 14:27:02 -0600

    updated for python3

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# dotw
+# dotw 
+
 Day of the Week Calculator
 
 Given a day, month, and year, this program will use an algorithm to figure out the day of the week in the Gregorian Calendar.
+
+usage:
+
+
+
--- dotw.py
+++ dotw.py
@@ -1,137 +1,129 @@
-# init date at 0
-given_day   = 0
-given_month = 0
-given_year  = 0
+#!/usr/bin/env python3
+"""
+dotw.py
+~~~~~~~
 
-# days of the week in base 7
-day_of_week = {
-	0:'sunday',
-	1:'monday',
-	2:'tuesday',
-	3:'wednesday',
-	4:'thursday',
-	5:'friday',
-	6:'saturday',
-}
+Python based calculator that determines the weekday of a given day
 
-# month values
-value_month = {
-	1:0,
-	2:3,
-	3:3,
-	4:6,
-	5:1,
-	6:4,
-	7:6,
-	8:2,
-	9:5,
-	10:0,
-	11:3,
-	12:5,
-}
+Credit: https://blog.artofmemory.com/how-to-calculate-the-day-of-the-week-4203.html
+"""
 
-length_month = {
-	1:31,
-	2:28,
-	3:31,
-	4:30,
-	5:31,
-	6:30,
-	7:31,
-	8:31,
-	9:30,
-	10:31,
-	11:30,
-	12:31,
-}
+def calc_dotw(given_month=0,given_day=0,given_year=1749):
+	""" Function that returns the day of the week given (mm,dd,ccyy) """
 
-# century values
-value_century = {
-	17:4,
-	18:2,
-	19:0,
-	20:6,
-	21:4,
-	22:2,
-	23:0,
-	24:6,
-}
+	day_of_week = {
+		0:'sunday',
+		1:'monday',
+		2:'tuesday',
+		3:'wednesday',
+		4:'thursday',
+		5:'friday',
+		6:'saturday',
+	}
 
-# promt user for mm/dd/ccyy
-while given_month not in range(1,13):
-	given_month = input("What month?(1-12) ")
+	value_month = {
+		1:0,
+		2:3,
+		3:3,
+		4:6,
+		5:1,
+		6:4,
+		7:6,
+		8:2,
+		9:5,
+		10:0,
+		11:3,
+		12:5,
+	}
 
-while given_day not in range(1,length_month[given_month]+1):
-	given_day = input("What day?  (1-31) ")
+	length_month = {
+		1:31,
+		2:29,
+		3:31,
+		4:30,
+		5:31,
+		6:30,
+		7:31,
+		8:31,
+		9:30,
+		10:31,
+		11:30,
+		12:31,
+	}
 
-while given_year < 1750:
-	given_year = input("What year? (ccyy) ")
+	value_century = {
+		17:4,
+		18:2,
+		19:0,
+		20:6,
+		21:4,
+		22:2,
+		23:0,
+		24:6,
+	}
 
-print "\n" + " %s / %s / %s" % (given_month, given_day, given_year)
+	# Promt user for mm/dd/ccyy within the range of the Gregorian Calendar
+	while given_month not in range(1,13):
+		given_month = int(input("What month?(1-12) "))
 
-# value_day / reduce
-value_day = given_day % 7
-# value_mm is returned from dict reference
-value_mm = value_month[given_month]
+	while given_day not in range(1,(length_month[given_month]+1)):
+		given_day = int(input("What day?  (1-31) "))
 
-# value_cc for part of year
-year = str(given_year)
-cc = int(year[:2])
-yy = int(year[2:])
-value_cc = value_century[cc]
+	while given_year < 1750:
+		given_year = int(input("What year? (ccyy) "))
 
-# value_yy using formula (yy + (yy div 4)) mod 7
-yy_div4  = yy / 4
-value_yy = (yy + yy_div4) % 7
+	value_day = given_day % 7
+	value_mm = value_month[given_month]
+	year = str(given_year)
 
-# consider leap_mm and leap_yy
-is_LeapYear = False
-value_ly = 0
-if given_month < 3:
-	# is within range of leap months
-	if given_year % 4 == 0:
-		# is divisible by four
-		if given_year % 100 == 0:
-			# is divisible by 100
-			if given_year % 400 == 0:
-				# is divisible by 400
-				# is leap year
+	# Calculate Value for Year (ccyy)
+	cc = int(year[:2]) # Century Code value_cc for part of year
+	value_cc = value_century[cc]
+
+	yy = int(year[2:]) # Year Code value_yy using formula (yy + (yy div 4)) mod 7
+	yy_div4  = yy // 4
+	value_yy = (yy + yy_div4) % 7
+
+	# consider leap_mm and leap_yy
+	is_LeapYear = False
+	value_ly = 0
+	if given_month < 3:
+		if given_year % 4 == 0:
+			if given_year % 100 == 0:
+				if given_year % 400 == 0:
+					value_ly = -1
+					is_LeapYear = True
+				else:
+					is_LeapYear = False
+			else:
 				value_ly = -1
 				is_LeapYear = True
-			else:
-				# is NOT divisibile by 400
-				# is NOT leap year
-				is_LeapYear = False
 		else:
-			# is NOT divisible by 100
-			# is leap year
-			value_ly = -1
-			is_LeapYear = True
+			is_LeapYear = False
 	else:
-		# is NOT leap year
 		is_LeapYear = False
-else:
-	# is NOT within range of leap months
-	is_LeapYear = False
 
-# value_year of using yy, cc and ly
-value_year = (value_cc + value_yy + value_ly)
+	# value_year of using yy, cc and ly
+	value_year = (value_cc + value_yy + value_ly)
 
-# total
-total = 0
-total += value_mm
-total += value_day
-total += value_year
+	# total
+	total = 0
+	total += value_mm
+	total += value_day
+	total += value_year
 
-# user side math
-# print " %s +  %s +    %s = %s" % (value_mm, value_day, value_year, total)
+	# Optional user side math
+	#print("\n" + " {} / {} / {}".format(given_month, given_day, given_year))
+	#print(" {} +  {} +    {} = {}".format(value_mm, value_day, value_year, total))
 
-# final_total
-final_total = total
+	# Ensure final total (day) fits within week structure
+	final_total = total
+	if final_total > 6:
+		final_total = final_total % 7
 
-if final_total > 6:
-	final_total = final_total % 7
+	day = day_of_week[final_total]
+	return day
 
-day = day_of_week[final_total]
-#print "\n" + "%s returns" % final_total
-print "was a %s" % day
+if __name__=="__main__":
+	day = calc_dotw(1,1,1990)
+	print("\n{}\n".format(day))
